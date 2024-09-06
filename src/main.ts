b3b85f60bf64bbaa49cf513b99f7567715ebdfc7
ChrstianH <henschel_christian@web.de>
2024-09-06 11:53:06 +0200

    TYpescript Teil 1

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,57 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import { INews, IArticle } from "./interfaces/INews";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const apiKey = "517e77f5cd1c4fc39b5eff509379de62";
+const BASE_URL = "https://newsapi.org";
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const formElement = document.getElementById("form") as HTMLFormElement;
+const searchElement = document.getElementById("search") as HTMLInputElement;
+const languageElement = document.getElementById(
+  "language"
+) as HTMLSelectElement;
+const sortElement = document.getElementById("sort") as HTMLSelectElement;
+const submitBtn = document.getElementById("btn-search") as HTMLButtonElement;
+
+formElement.appendChild(searchElement);
+formElement.appendChild(languageElement);
+formElement.appendChild(sortElement);
+formElement.appendChild(submitBtn);
+
+const newsContent = document.getElementsByClassName(
+  "news-content"
+)[0] as HTMLDivElement;
+
+const createNewsCard = () => {
+  const newsCard = document.createElement("div") as HTMLDivElement;
+  newsCard.className = "news-card";
+  const headlineContent = document.createElement("h2") as HTMLHeadingElement;
+  headlineContent.className = "title";
+  const infoElement = document.createElement("p") as HTMLParagraphElement;
+  infoElement.className = "info";
+  const imageElement = document.createElement("img") as HTMLParagraphElement;
+  const toArticleElement = document.createElement(
+    "button"
+  ) as HTMLButtonElement;
+  toArticleElement.className = "to-article";
+};
+formElement.addEventListener("submit", (event: SubmitEvent) => {
+  event.preventDefault();
+  let newsURL = `${BASE_URL}/v2/everything`;
+
+  const queryParams: string[] = [];
+  if (searchElement.value.length !== 0) {
+    const searchKey = searchElement.value;
+    queryParams.push(`q=${searchKey}`);
+  }
+
+  if (languageElement.value.length !== 0) {
+    const languageKey = languageElement.value;
+    queryParams.push(`language=${languageKey}`);
+  }
+
+  if (sortElement.value.length !== 0) {
+    const sortKey = sortElement.value;
+    queryParams.push(`sortBy=${sortKey}`);
+  }
+
+  newsURL += `?${queryParams.join("&")}`;
+});
